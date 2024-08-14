2747497044901e70f96dd0280f2283f9874610f0
Nehal Kadyan <89070793+nehalkadyan@users.noreply.github.com>
2024-08-14 23:56:47 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import UserAuth from "../components/userAuth";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Register = () => {
+  const navigate = useNavigate();
+
+  const { token } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+    }
+  }, [token]);
+
   return (
     <div>
       <UserAuth
